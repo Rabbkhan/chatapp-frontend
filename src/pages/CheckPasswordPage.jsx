bdd5422ffcc96a41745000235ad219200f41d5d7
Rabbilkhan <rabbilkhan125@gmail.com>
2024-06-22 04:37:18 +0530

    changepassword.js changes

--- src/pages/CheckPasswordPage.jsx
+++ src/pages/CheckPasswordPage.jsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { IoCloseOutline } from "react-icons/io5";
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import uploadFile from '../helpers/upload';
-import axios from 'axios'
-import { FaRegCircleUser } from "react-icons/fa6";
+import axios from 'axios';
 import Avatar from '../components/Avatar';
 import { useDispatch } from 'react-redux';
 import { setToken, setUser } from '../redux/userSlice';
@@ -12,87 +10,77 @@ import toast from 'react-hot-toast';
 const CheckPasswordPage = () => {
   const [data, setData] = useState({
     password: "",
-    userId : ""
-
+    userId: ""
   });
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const location = useLocation();
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+
   useEffect(() => {
     if (!location?.state?.name) {
       navigate('/email');
     }
-  }, []);
+  },[]);
 
-  // const basepath =location.pathname === '/'
   const handleOnChange = (e) => {
     const { name, value } = e.target;
-
-    setData((prev) => {
-
-      return {
-        ...prev,
-        [name]: value
-      }
-    });
+    setData((prev) => ({
+      ...prev,
+      [name]: value
+    }));
   };
-
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     e.stopPropagation();
 
-    const URL = `${import.meta.env.VITE_APP_BACKEND_URL}/api/password`
+    const URL = `${import.meta.env.VITE_APP_BACKEND_URL}/api/password`;
 
     try {
-      const response = await axios({
-        method: 'post',
-        url: URL,
-        data: {
-          userId: location?.state?._id,
-          password: data.password
-        },
+      const response = await axios.post(URL, {
+        userId: location?.state?._id,
+        password: data.password
+      }, {
         withCredentials: true
-      })
+      });
 
-      toast.success(response.data.message)
+      toast.success(response.data.message);
 
       if (response.data.success) {
-       dispatch(setToken(response?.data?.token))
-        localStorage.setItem('token', response?.data?.token)
-        setData({
-          password: "",
-        })
-        navigate('/')
+        dispatch(setToken(response?.data?.token));
+        localStorage.setItem('token', response?.data?.token);
+        setData({ password: "" });
+        navigate('/');
       }
     } catch (error) {
-      toast.error(error?.response?.data?.message)
+      toast.error(error?.response?.data?.message);
     }
   };
 
-  // console.log("data", data)
+  // Debugging logs
+  useEffect(() => {
+    console.log("location.state:", location.state);
+    console.log("data:", data);
+  }, [location, data]);
+
   return (
     <div className='mt-5'>
-
-      <div className='bg-white w-full max-w-md  rounded overflow-hidden p-4 mx-auto'>
+      <div className='bg-white w-full max-w-md rounded overflow-hidden p-4 mx-auto'>
         <div className='w-fit mx-auto mt-3 flex justify-center items-center flex-col'>
-          {/* <FaRegCircleUser size={80}/> */}
           <Avatar
             height={70}
             width={70}
             name={location?.state?.name}
-            imageUrl={location?.state?.profile_pic
-            }
+            imageUrl={location?.state?.profile_pic}
           />
           <h2 className='text-center'>{location?.state?.name}</h2>
         </div>
 
         <h3>Login to Chat App!</h3>
         <form className='grid gap-1 mt-5' onSubmit={handleSubmit}>
-
           <div className='flex flex-col gap-1'>
-            <label htmlFor="password">password:</label>
+            <label htmlFor="password">Password:</label>
             <input
               type='password'
               id='password'
@@ -104,14 +92,12 @@ const CheckPasswordPage = () => {
               required
             />
           </div>
-
-
-
-
           <button className='bg-primary text-lg px-4 py-2 text-white hover:bg-secondary rounded-sm mt-4 font-bold leading-relaxed tracking-wide'>Login</button>
         </form>
 
-        <p className='my-2'><Link to={'/forgot-password'} className='text-primary hover:underline font-semibold'>Forgot Password</Link></p>
+        <p className='my-2'>
+          <Link to={'/forgot-password'} className='text-primary hover:underline font-semibold'>Forgot Password</Link>
+        </p>
       </div>
     </div>
   );
